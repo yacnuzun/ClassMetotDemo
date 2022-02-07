e260710d517c234de23c28cb317f3f18dfe9b4be
Yalçın Uzun <yacn.uzun@gmail.com>
2022-02-08 02:48:02 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri[] musteriler)
+        {
+            Console.WriteLine(musteriler+" Added!!");
+        }
+
+        public void View(Musteri [] musteriler)
+        {
+            Console.WriteLine(musteriler);
+        }
+        public void Delete(Musteri[] musteriler)
+        {
+            Console.WriteLine(musteriler+" Deleted!!");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Miko";
+            musteri1.Soyad = "Gurler";
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Selahittin";
+            musteri2.Soyad = "Kısazade";
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "İsmail";
+            musteri3.Soyad = "Abi";
+            Musteri [] musteriler=new Musteri[] { musteri1,musteri2,musteri3 };
+            MusteriManager musteriManager = new MusteriManager();
+            foreach (Musteri musteri in musteriler)
+            {
+                musteriManager.View(musteri);
+            }
+            
+            Console.Read();
+        }
+    }
+}
